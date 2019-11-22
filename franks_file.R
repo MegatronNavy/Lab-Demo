a0d09ac83d6ec55f02922d993836d90bb7fba927
MegatronNavy <ianzhengzorro@gmail.com>
2019-11-22 14:59:23 -0800

    sd=2

--- franks_file.R
+++ franks_file.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 
-tibble(x = rnorm(100), y = rnorm(100)) %>%
+tibble(x = rnorm(100,sd=2), y = rnorm(100,sd=2)) %>%
     ggplot() +
     geom_point(aes(x=x, y=y))
-                           
